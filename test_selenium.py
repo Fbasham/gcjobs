2795a6f9ea612ea2bd585806a2ef085a08b86c63
Fbasham <48450599+Fbasham@users.noreply.github.com>
2020-08-26 19:42:27 -0400

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -8,7 +8,7 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
-driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 driver.get('https://emploisfp-psjobs.cfp-psc.gc.ca/psrs-srfp/applicant/page2440?fromMenu=true&toggleLanguage=en')
 print(driver.page_source)
