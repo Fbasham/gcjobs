53580143902b2ff6b583069608a0996dfcebc974
Fbasham <48450599+Fbasham@users.noreply.github.com>
2020-08-26 19:55:27 -0400

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -7,6 +7,6 @@ option.add_argument("--headless")
 option.add_argument('--disable-gpu')
 option.add_argument('--no-sandbox')
 
-driver = webdriver.Chrome(executable_path=os.getenv('CHROME_EXECUTABLE_PATH'), options=option)
+driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH'), options=option)
 driver.get('https://emploisfp-psjobs.cfp-psc.gc.ca/psrs-srfp/applicant/page2440?fromMenu=true&toggleLanguage=en')
 print(driver.page_source)
