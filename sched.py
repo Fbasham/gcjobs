87cd487c4b1b18247046aebbadf0f2565b0ae59b
Fbasham <48450599+Fbasham@users.noreply.github.com>
2020-08-28 07:51:53 -0400

    Update sched.py

--- sched.py
+++ sched.py
@@ -1,9 +1,9 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 import scrape
 
-scheduler = BackgroundScheduler()
+scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', hours=1, start_date='2020-08-28 11:50:00')
+@scheduler.scheduled_job('interval', hours=1, start_date='2020-08-28 12:00:00')
 def job():
     print('running scheduled scrape')
     scrape.main()
