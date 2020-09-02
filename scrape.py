f77ac15e3bec7584c9c3fe6284dde979019b0bc6
Fbasham <48450599+Fbasham@users.noreply.github.com>
2020-09-02 10:27:13 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -17,34 +17,53 @@ def main():
 
     driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH'), options=option)
     wait_for_tag = lambda tag: WebDriverWait(driver,30).until(lambda x: x.find_element_by_css_selector(tag))
-
+    
+    USERNAME = os.getenv('GCJOBS_USERNAME')
+    PASSWORD = os.getenv('GCJOBS_PASSWORD')
     DOMAIN = 'https://emploisfp-psjobs.cfp-psc.gc.ca'
     CACHE = []
    
     try:
-        driver.get(f'{DOMAIN}/psrs-srfp/applicant/page2440?requestedPage=1&tab=2')
-        wait_for_tag('span.pagelinks')
+        
+        #login
+        is_authenticated = False
+        try:
+            driver.get('https://emploisfp-psjobs.cfp-psc.gc.ca/psrs-srfp/applicant/page1710')
+            driver.find_element_by_id('UserNumber').send_keys(USERNAME)
+            driver.find_element_by_id('Password').send_keys(PASSWORD)
+            driver.find_element_by_css_selector('input[name=LOGIN]').click()
+            is_authenticated = True
+            
+        except Exception as e:
+            print(e)
+            
+        tabs = (1,2) if is_authenticated else (1,)
+        for tab in tabs:
+            driver.get(f'{DOMAIN}/psrs-srfp/applicant/page2440?requestedPage=1&tab={tab}')
+            wait_for_tag('span.pagelinks')
+            
+            total_pages = int(re.findall('of (\d+)',driver.page_source)[0])
 
-        total_pages = int(re.findall('of (\d+)',driver.page_source)[0])
+            for n in range(1,total_pages+1):
+                driver.get(f'{DOMAIN}/psrs-srfp/applicant/page2440?requestedPage={n}&tab={tab}')
+                #wait_for_tag('span.pagelinks')
 
-        for n in range(1,total_pages+1):
-            driver.get(f'{DOMAIN}/psrs-srfp/applicant/page2440?requestedPage={n}&tab=2')
-            #wait_for_tag('span.pagelinks')
+                soup = BeautifulSoup(driver.page_source,'html.parser')
+                results = soup.select('li.searchResult')
 
-            soup = BeautifulSoup(driver.page_source,'html.parser')
-            results = soup.select('li.searchResult')
+                for row in results:
+                    x = row.select_one('a')
+                    url,title = x.attrs.get('href'), x.text
+                    date,dept,*loc = list(row.select_one('div.tableCell').stripped_strings)
 
-            for row in results:
-                x = row.select_one('a')
-                url,title = x.attrs.get('href'), x.text
-                date,dept,*loc = list(row.select_one('div.tableCell').stripped_strings)
-
-                CACHE.append({'url': f'{DOMAIN}{url}',
-                              'title': title,
-                              'closing': date.replace('Closing date: ',''),
-                              'department': dept.split(' - ')[0].strip(),
-                              'location': ','.join(loc)}
-                )
+                    CACHE.append({'url': f'{DOMAIN}{url}',
+                                  'title': title,
+                                  'closing': date.replace('Closing date: ',''),
+                                  'department': dept.split(' - ')[0].strip(),
+                                  'location': ','.join(loc),
+                                  'internal': 1 if tab==1 and is_authenticated else 0
+                                  }
+                                 )
 
     except Exception as e:
         print(e)
@@ -75,6 +94,8 @@ def main():
 
     async def main():    
         s = AsyncHTMLSession()
+        data = {'UserNumber': USERNAME, 'Password': PASSWORD, 'LOGIN': 'Login'}
+        await s.post('https://emploisfp-psjobs.cfp-psc.gc.ca/psrs-srfp/applicant/page1710', data=data)
         tasks = (scrape(s,d) for d in CACHE)
         return await asyncio.gather(*tasks)
 
@@ -88,13 +109,13 @@ def main():
             ROWS.append(d)
             SEEN.add(key)
 
-    DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASE_URL = os.getenv('DATABASE_URL')
     if ROWS:
         with psycopg2.connect(DATABASE_URL, sslmode='require') as conn:
             c = conn.cursor()
             c.execute('drop table if exists job;')
-            c.execute('CREATE TABLE if not exists job (id serial primary key, url text, title text, closing text, department text, location text, contents text);')
-            c.executemany('insert into job(url, title, closing, department, location, contents) values (%s,%s,%s,%s,%s,%s)', (tuple(d.values()) for d in ROWS))
+            c.execute('CREATE TABLE if not exists job (id serial primary key, url text, title text, closing text, department text, location text, internal smallint, contents text);')
+            c.executemany('insert into job(url, title, closing, department, location, contents) values (%s,%s,%s,%s,%s,%s,%s)', (tuple(d.values()) for d in ROWS))
             conn.commit()
         print('data written to postgres')
     else:
