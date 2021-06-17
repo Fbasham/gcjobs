c0204282b485cf988a7461ace1173def3cceb04e
Fbasham <48450599+Fbasham@users.noreply.github.com>
2021-06-17 07:45:48 -0400

    Update gcjobsapp.py

--- gcjobsapp.py
+++ gcjobsapp.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def inject_year():
     return dict(year=datetime.now().year)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
